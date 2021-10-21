21db8bc2e54a115e5d928a60941a50186e819c19
mihai10001 <rvivits@gmail.com>
2021-10-21 15:09:11 +0300

    add ngx-perfect-scrollbar library for better scrollbar

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +21,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    PerfectScrollbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
