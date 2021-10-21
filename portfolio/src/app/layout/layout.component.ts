2805c6375b26550417e1c617fb1b7b670bcadcbf
mihai10001 <rvivits@gmail.com>
2021-10-21 15:13:00 +0300

    temporary 3D object rendering configuration and CSS for layout

--- portfolio/src/app/layout/layout.component.ts
+++ portfolio/src/app/layout/layout.component.ts
@@ -1,6 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+import { Inject} from "@angular/core";
+import { DOCUMENT } from '@angular/common';
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-layout',
@@ -10,34 +14,80 @@ import * as THREE from 'three';
 export class LayoutComponent implements  AfterViewInit {
 
   @ViewChild('canvas') canvas: ElementRef;
+  threeScene = new THREE.Scene();
+  threeRenderer = new THREE.WebGLRenderer();
+  threeCamera = new THREE.PerspectiveCamera( 75, 0, 0.1, 1000 );
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2
   ) { }
 
+
+  @HostListener('wheel', ['$event'])
+  public onWheel(event) {
+    if (event.deltaY < 0) {
+      this.threeCamera.position.z -= 1;
+    }
+    else if (event.deltaY > 0) {
+      this.threeCamera.position.z += 1;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(){
+    this.threeCamera.aspect = window.innerWidth / window.innerHeight;
+    this.threeCamera.updateProjectionMatrix();
+    this.threeRenderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
   ngAfterViewInit(): void {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, this.canvas.nativeElement.offsetWidth/this.canvas.nativeElement.offsetHeight, 0.1, 1000 );
+    this.threeCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    var renderer = new THREE.WebGLRenderer();
+    this.threeRenderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.appendChild(this.canvas.nativeElement, this.threeRenderer.domElement);
+    this.threeCamera.position.z = 20;
 
-    renderer.setSize( this.canvas.nativeElement.offsetWidth, this.canvas.nativeElement.offsetHeight );
-    this.renderer.appendChild(this.canvas.nativeElement, renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    const geometry = new THREE.BoxGeometry(5, 5, 5);
+    const material = new THREE.MeshStandardMaterial({ color: 0xFF55555 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(-10, 0, 0);
+    this.threeScene.add(cube);
 
-    camera.position.z = 5;
+    const pointLight = new THREE.PointLight(0xfffffff);
+    pointLight.position.set(5, 5, 5);
 
-    var animate = function () {
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    this.threeScene.add(pointLight, ambientLight);
+
+    // const lightHelper = new THREE.PointLightHelper(pointLight)
+    // const gridHelper = new THREE.GridHelper(200, 50);
+    // this.threeScene.add(lightHelper, gridHelper);
+
+    // const controls = new OrbitControls(this.threeCamera, this.threeRenderer.domElement);
+    // controls.enablePan = false;
+    // controls.enableRotate = false;
+
+    Array(400).fill(0).forEach(() => this.addStar(this.threeScene));
+
+    const animate = () => {
       requestAnimationFrame( animate );
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
+      this.threeRenderer.render(this.threeScene, this.threeCamera);
     };
 
     animate();
+  }
+
+  addStar(scene) {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill(0).map(() => THREE.MathUtils.randFloatSpread(200));
+    star.position.set(x, y, z);
+    scene.add(star);
   }
 }
