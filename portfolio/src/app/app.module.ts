c4d76c0b04ed883d8400be85147035ea88f9653d
mihai10001 <rvivits@gmail.com>
2021-10-26 14:03:53 +0300

    remove unused ngx-perfect-scrollbar

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
@@ -20,8 +18,6 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    PerfectScrollbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
