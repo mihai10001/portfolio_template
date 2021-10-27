4456f6e21cf0a67f035975f70d639995948bb88a
mihai10001 <rvivits@gmail.com>
2021-10-27 22:43:22 +0300

    missing material imports

--- portfolio/src/app/material.module.ts
+++ portfolio/src/app/material.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
-
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     MatCardModule,
     MatButtonModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatIconModule
   ],
   exports: [
     MatCardModule,
     MatButtonModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatIconModule
   ]
 })
 export class MaterialModule {}
