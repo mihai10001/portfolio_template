3f41f3ce9a5e675e17cd4acf5fb9b15fc2ffb689
mihai10001 <rvivits@gmail.com>
2021-10-27 22:41:36 +0300

    create custom mat-card component

---
+++ portfolio/src/app/content/mat-card/mat-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-card',
+  templateUrl: './mat-card.component.html',
+  styleUrls: ['./mat-card.component.css']
+})
+export class MatCardComponent implements OnInit {
+
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() avatarMatIcon: string = '';
+  @Input() content: string = '';
+  @Input() imgSrc: string = '';
+
+  @Input() gitButtonLink: string = '';
+  @Input() demoButtonLink: string = '';
+  @Input() customButtonLink: string = '';
+  @Input() customButtonText: string = '';
+
+  @Input() tags: string[] = [];
+  @Input() disabled: boolean = false;
+
+  expanded: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  toggleExpandedContent = () => this.expanded = !this.expanded;
+}
