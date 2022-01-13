379e8cbb760435a053d75f5aa6eca5d38b628a8d
mihai10001 <rvivits@gmail.com>
2022-01-13 18:55:59 +0200

    new paragraph card model, add banner title and subtitle fields to content model

--- portfolio/src/app/content/models/CardModel.ts
+++ portfolio/src/app/content/models/CardModel.ts
@@ -10,3 +10,17 @@ export class CardModel {
     tags: Array<string>;
     disabled: boolean;
 }
+
+export class ParagraphCardModel {
+    title: string;
+    description: string;
+    paragraphList: Array<Paragraph>;
+    isParagraph: boolean = true;
+}
+
+class Paragraph {
+    content: string;
+    link: string;
+    icon: string;
+}
+  
--- portfolio/src/app/content/models/ContentModel.ts
+++ portfolio/src/app/content/models/ContentModel.ts
@@ -1,5 +1,7 @@
 import { SectionModel } from './SectionModel';
 
 export class ContentModel {
+    bannerTitle: string;
+    bannerSubtitle: string;
     sections: Array<SectionModel>;
 }
