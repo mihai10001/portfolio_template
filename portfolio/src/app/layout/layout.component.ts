bbded01fa13007d8907be6f9284166bb5ed30f41
mihai10001 <rvivits@gmail.com>
2022-03-25 16:00:21 +0200

    implement object transitions: spin group object when switching content tabs

--- portfolio/src/app/layout/layout.component.ts
+++ portfolio/src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 import * as THREE from 'three';
+import TWEEN from '@tweenjs/tween.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
@@ -33,7 +34,10 @@ export class LayoutComponent implements  AfterViewInit {
   objectColor: number = 0x5DADE2;
   zCameraOffset: number = 50;
   xObjectOffset: number = -13;
-  orbitalControls: boolean = false;
+  sphereRadius: number = 30;
+  thetaObjectDegreeOffset: number = 0;
+  spinByRadians: number = 0;
+
   content: ContentModel = this.contentService.content;
   object3DArray: Array<{ index: number, object: THREE.Object3D, deltaDegreesToCenter: number }> = [];
 
@@ -56,20 +60,28 @@ export class LayoutComponent implements  AfterViewInit {
 
     this.helpers && this.addGridHelper(200, 50);
     this.postProcessing && this.enablePostProcessing();
-    this.orbitalControls && this.enableOrbitalControls();
+
+    this.contentService.activeTabIndexSubject
+      .subscribe(activeTabIndex =>
+        this.spinObjectTween(objectGroup, this.spinObjectGroupByRadians(activeTabIndex))
+      )
+
 
     const animate = () => {
       requestAnimationFrame(animate);
+      TWEEN.update();
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      for(let i = 0; i < starContainers.length; i++)
+      for (let i = 0; i < objectGroup.children.length; i++) {
+        objectGroup.children[i].rotation.x += 0.004;
+        objectGroup.children[i].rotation.y += 0.004;
+      }
+      for (let i = 0; i < starContainers.length; i++)
         starContainers[i].rotation.y += 0.0001;
 
       !this.postProcessing && this.threeRenderer.render(this.threeScene, this.threeCamera);
       this.postProcessing && this.threeComposer.render(this.threeScene, this.threeCamera);
     };
-  
+
     animate();
   }
 
@@ -104,6 +116,7 @@ export class LayoutComponent implements  AfterViewInit {
   }
 
   addObjectGroup(helpers: boolean): THREE.Group {
+    const degreesBetweenObjects = Math.floor(360 / this.content.sections.length);
     const group = new THREE.Group();
     const sphere = new THREE.Mesh(
       new THREE.SphereGeometry(this.sphereRadius, 32, 32),
@@ -111,10 +124,38 @@ export class LayoutComponent implements  AfterViewInit {
     );
     helpers && this.threeScene.add(sphere);
 
+    for (let i = 0; i < this.content.sections.length; i++) {
+      let currentDegreeOffset = (i * degreesBetweenObjects) + this.thetaObjectDegreeOffset;
+
+      if (this.content.sections[i].object3DPath === undefined) {
+        const cube = this.createCube(8, 8, 8, this.objectColor);
+        this.objectColor += 0x33333;
+        cube.position.setFromSphericalCoords(this.sphereRadius + 0.1, THREE.Math.degToRad(90), THREE.Math.degToRad(currentDegreeOffset));
+        cube.lookAt(sphere.position);
+        this.object3DArray.push({ index: i, object: cube, deltaDegreesToCenter: currentDegreeOffset });
+        group.add(cube);
+      }
+    }
 
     group.position.set(this.xObjectOffset, 0, 0);
     this.threeScene.add(group);
     return group;
+  }
+
+
+  spinObjectGroupByRadians(activeTabIndex: number): number {
+    const activeObject = this.object3DArray.find(entry => entry.index === activeTabIndex);
+    return activeObject
+      ? THREE.Math.degToRad(360 - activeObject.deltaDegreesToCenter + this.thetaObjectDegreeOffset)
+      : 0;
+  }
+
+
+  spinObjectTween(group: THREE.Group, spinByRadians: number, delay: number = 500) {
+    new TWEEN.Tween(group.rotation)
+      .to({ y: spinByRadians }, delay)
+      .easing(TWEEN.Easing.Cubic.InOut)
+      .start();
   }
 
   createCube(cubeWidth: number, cubeHeight: number, cubeDepth: number, cubeColor: number, xCubeOffset: number = 0) {
