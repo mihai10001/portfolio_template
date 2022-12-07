1e1dbe2f52d94c5358931a1cd7827540674038eb
mihai10001 <rvivits@gmail.com>
2022-12-07 22:24:57 +0200

    missing imports

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material.module';
 import { LayoutComponent } from './layout/layout.component';
 import { ContentModule } from './content/content.module';
 
@@ -19,6 +20,7 @@ import { ContentModule } from './content/content.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     ContentModule,
+    MaterialModule,
     AngularEmojisModule
   ],
   providers: [],
--- portfolio/src/app/content/content.module.ts
+++ portfolio/src/app/content/content.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularEmojisModule } from 'angular-emojis';
 import { SwipeModule } from 'ng-swipe';
 
 import { MaterialModule } from '../material.module';
 import { ContentComponent } from './content.component';
 import { MatCardComponent } from './mat-card/mat-card.component';
+import { MatParagraphCardComponent } from './mat-paragraph-card/mat-paragraph-card.component';
 
 
 @NgModule({
   declarations: [
     ContentComponent,
     MatCardComponent,
+    MatParagraphCardComponent,
   ],
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AngularEmojisModule,
     SwipeModule,
   ]
 })
